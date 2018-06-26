8f7632f956084d927bca99819d22625f9485fc4b
Miquel Casals <mcasals@covalco.es>
2018-06-26 16:04:50 +0200

    Afegeixo la documentació de classes del projecte PooVueling utilitzant la utilitat SandCastle. L'ajuda l'he generat en format .chm

--- PooVueling/Calculadora.cs
+++ PooVueling/Calculadora.cs
@@ -8,43 +8,89 @@ namespace PooVueling
 {
 	public class Calculadora : ICalculadora
 	{
+		/// <summary>
+		/// Divisions the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public int Division( int num1, int num2 )
 		{
 			return(num1 / num2);
 		}
 
+		/// <summary>
+		/// Multiplicacions the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public int Multiplicacion( int num1, int num2 )
 		{
 			return(num1 * num2);
 		}
 
+		/// <summary>
+		/// Restas the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public int Resta( int num1, int num2 )
 		{
 			return(num1 - num2);
 		}
 
+		/// <summary>
+		/// Sumas the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public int Suma( int num1, int num2 )
 		{
 			return(num1 + num2);
 		}
-		///
+		/// <summary>
+		/// Divisions the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		/// Mètodes amb doubles
-		/// 
 		public double Division( double num1, double num2 )
 		{
 			return(num1 / num2);
 		}
 
+		/// <summary>
+		/// Multiplicacions the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public double Multiplicacion( double num1, double num2 )
 		{
 			return(num1 * num2);
 		}
 
+		/// <summary>
+		/// Restas the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public double Resta( double num1, double num2 )
 		{
 			return(num1 - num2);
 		}
 
+		/// <summary>
+		/// Sumas the specified num1.
+		/// </summary>
+		/// <param name="num1">The num1.</param>
+		/// <param name="num2">The num2.</param>
+		/// <returns></returns>
 		public double Suma( double num1, double num2 )
 		{
 			return(num1 + num2);
